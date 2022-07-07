7e5c87cbd28a59280c5781bf3f95f200006ba1a4
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-07-07 19:51:01 +0600

    Lab 6 updated

--- Lab6.sql
+++ Lab6.sql
@@ -1,15 +1,19 @@
--- -- Join
--- select p.property_id, p.price, s.name
--- from property p join seller s
--- using (seller_id);
+-- Join
+select p.property_id, p.price, s.name
+from property p join seller s
+using (seller_id);
 
 
--- -- Natural Join
--- select agent_id, client_id
--- from agent natural join client;
+-- Natural Join
+select appointment_id, appointment_date, client_id
+from appointment natural join client;
 
 
 -- Inner Join
-select a.agent_id, a.name, c.name, client_id
-from agent a inner join client c
+select a.appointment_id, a.appointment_date, c.name, client_id
+from appointment a inner join client c
 using (client_id);
+
+-- Cross Join
+select s.selling_info_id, s.sdate, se.name
+from selling_info s cross join seller se;
