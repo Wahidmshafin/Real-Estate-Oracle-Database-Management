39321336632de5fec03236eda2fc8c93e57da2c4
Shafin <mahmood1807074@stud.kuet.ac.bd>
2022-07-07 20:02:42 +0600

    Lab6 completed

--- Lab6.sql
+++ Lab6.sql
@@ -10,10 +10,30 @@ from appointment natural join client;
 
 
 -- Inner Join
-select a.appointment_id, a.appointment_date, c.name, client_id
-from appointment a inner join client c
-using (client_id);
+select s.sdate, se.name, s.client_id, s.property_id
+from selling_info s inner join seller se
+using(seller_id);
+
+
 
 -- Cross Join
 select s.selling_info_id, s.sdate, se.name
 from selling_info s cross join seller se;
+
+
+-- Left Outer Join
+select s.sdate, se.name, s.client_id, s.property_id
+from selling_info s left outer join seller se
+using(seller_id);
+
+
+-- Right Outer Join
+select s.sdate, se.name, s.client_id, s.property_id
+from selling_info s right outer join seller se
+using(seller_id);
+
+
+-- Full Outer Join
+select s.sdate, se.name, s.client_id, s.property_id
+from selling_info s full outer join seller se
+using(seller_id);
